7f160bbb29589edcdaa0bbb054564cbc64574371
Yusuf YILMAZ <ysfyilmz@outlook.com>
2019-07-25 11:38:10 +0300

    Add files via upload

---
+++ WiFiAccessPoint.ino
@@ -0,0 +1,33 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+IPAddress locip(192,168,4,1);
+IPAddress hostip(192,168,4,10);
+IPAddress subnet(255,255,255,0);
+WiFiUDP Udp;
+const char *ssid = "direk1";
+const char *password = "00000000";
+unsigned int localUdpPort=4210;
+char codec[50];
+void setup() {
+  Serial.begin(115200);
+  WiFi.config(hostip, locip, subnet);
+  WiFi.softAP(ssid, password,2);
+  Udp.begin(localUdpPort);
+}
+
+void loop() {
+   if(Serial.available()>0){
+      Serial.readBytes(codec,50);
+    Udp.beginPacket(Udp.remoteIP(),localUdpPort);
+    Udp.write(codec);
+    Udp.endPacket();
+    codec[50]={NULL};
+    }
+  int datalen=Udp.parsePacket();
+  if(datalen){
+    int len=Udp.read(codec,50);
+    Serial.write(codec);
+    if(len>0){codec[len]=0;}
+    }
+    
+    }
---
+++ kumanda.ino
@@ -0,0 +1,38 @@
+#include <ESP8266WiFi.h> 
+#include <WiFiClient.h>
+#include <WiFiUdp.h>
+const char* ssid= "direk1";
+const char* password="00000000";
+unsigned int localUdpPort=4210;
+char codec[50];
+IPAddress locip(192,168,4,1);
+IPAddress hostip(192,168,4,10);
+IPAddress subnet(255,255,255,0);
+WiFiUDP Udp;
+void setup() {
+ Serial.begin(115200);
+ WiFi.mode(WIFI_STA);
+ WiFi.config(hostip, locip, subnet);
+ 
+  WiFi.begin(ssid,password,2);
+  
+  Udp.begin(localUdpPort);
+ } 
+void loop() { 
+  while(WiFi.status() != WL_CONNECTED){delay(1);}   
+    if(Serial.available()>0){
+      Serial.readBytes(codec,50);
+    Udp.beginPacket(locip,localUdpPort);
+    Udp.write(codec);
+    Udp.endPacket();
+    codec[50]={NULL};
+    }
+  int datalen=Udp.parsePacket();
+  if(datalen){
+    int len=Udp.read(codec,50);
+    Serial.write(codec);
+    if(len>0){codec[len]=0;}
+    }
+  }
+
+
